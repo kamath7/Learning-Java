8de58e3b97565b27228b0161346057269da1e64a
kamath7 <adithyakamath96@gmail.com>
2022-02-15 14:12:52 +0530

    cosmetic changes

--- MethodOverloading/src/com/kamathinc/Main.java
+++ MethodOverloading/src/com/kamathinc/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("Default score - "+calculateScore(10));
         System.out.println("Score with multiplier "+calculateScore("Rooney",10, 2));
         System.out.println("No player no score - "+calculateScore());
-        System.out.println("Total cms for 5 foot 11 inches "+calcFeetAndInchesToCentimeters(5, 11));
+        System.out.println("Total cms "+calcFeetAndInchesToCentimeters(-10, 5));
     }
 // To overload you need to change the number of parameters. changing the return type won't do anything
     public static int calculateScore(String playerName, int score){
@@ -34,22 +34,23 @@ public class Main {
     }
 
     public static double calcFeetAndInchesToCentimeters(double feet, double inches){
-        if (feet >= 0.0 && inches >=0.0 ){
+        if (feet >= 0.0 && (inches >= 0 || inches < 12)){
             double totalInches = 0;
             totalInches = feet*12;
             totalInches += inches;
             return calcFeetAndInchesToCentimeters(totalInches);
         }
-
+        System.out.println("Invalid");
         return -1.0;
     }
 
     public static double calcFeetAndInchesToCentimeters(double inches){
 
         if(inches >= 0.0){
+
             return inches * 2.54;
         }
-
+        System.out.println("Invalid");
         return -1.0;
     }
 }
