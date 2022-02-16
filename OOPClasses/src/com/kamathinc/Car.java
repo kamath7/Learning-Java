32a3d179339ee9b0754281e7ac3d8bfd4fb1ba1c
kamath7 <adithyakamath96@gmail.com>
2022-02-17 02:03:59 +0530

    creating a car class

---
+++ OOPClasses/src/com/kamathinc/Car.java
@@ -0,0 +1,15 @@
+package com.kamathinc;
+
+/**
+ * Created by Ganesh on 17-02-2022.
+ */
+public class Car {
+    private int wheels;
+    private int doors;
+    private String engine;
+    private String color;
+    private String model;
+
+
+
+}
---
+++ OOPClasses/src/com/kamathinc/Main.java
@@ -0,0 +1,12 @@
+package com.kamathinc;
+
+public class Main {
+//Datatype on steroids
+    public static void main(String[] args) {
+	// write your code here
+        Car awesomeCar = new Car();
+        Car newCar = new Car();
+
+        System.out.println(awesomeCar.getClass());
+    }
+}
