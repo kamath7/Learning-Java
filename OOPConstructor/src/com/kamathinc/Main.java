fbeab8b803476ebb1f341ca62597cb84f6be334d
kamath7 <adithyakamath96@gmail.com>
2022-02-18 00:05:43 +0530

    constructors done

--- OOPConstructor/src/com/kamathinc/Main.java
+++ OOPConstructor/src/com/kamathinc/Main.java
@@ -21,5 +21,16 @@ public class Main {
 
         BankAccount anotherAcct = new BankAccount("Kams","kam@kam.com","+917867281909");
         System.out.println(anotherAcct.getCustomerName()+" has "+anotherAcct.getAccountBalance());
+
+        VipCustomer vipCustomer = new VipCustomer();
+        System.out.println(vipCustomer.getName()+ " has credit limit "+vipCustomer.getCreditLimit()+" and email is "+vipCustomer.getEmailAddress());
+
+        VipCustomer vipCustomer1 = new VipCustomer("Amitabh Bachan",420);
+        System.out.println(vipCustomer1.getName()+ " has credit limit "+vipCustomer1.getCreditLimit()+" and email is "+vipCustomer1.getEmailAddress());
+
+        VipCustomer vipCustomer2 = new VipCustomer("Jagga",100, "jagga@jagga.com");
+        System.out.println(vipCustomer2.getName()+ " has credit limit "+vipCustomer2.getCreditLimit()+" and email is "+vipCustomer2.getEmailAddress());
+
+
     }
 }
