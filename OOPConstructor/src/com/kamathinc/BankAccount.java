88b624f99057d872afd4dc28a518b94fec8e1279
kamath7 <adithyakamath96@gmail.com>
2022-02-17 17:03:09 +0530

    starting with constructors. initial boilerplate

---
+++ OOPConstructor/src/com/kamathinc/BankAccount.java
@@ -0,0 +1,52 @@
+package com.kamathinc;
+
+/**
+ * Created by Ganesh on 17-02-2022.
+ */
+public class BankAccount {
+    private int accountNumber;
+    private double accountBalance;
+    private String customerName;
+    private String emailAddress;
+    private String phoneNumber;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
