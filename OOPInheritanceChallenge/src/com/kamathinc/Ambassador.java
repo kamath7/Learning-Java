1080fc0a4c7cbe082352cbd8818a89a5af136b85
kamath7 <adithyakamath96@gmail.com>
2022-02-22 01:17:54 +0530

    testing inheritance

--- OOPInheritanceChallenge/src/com/kamathinc/Ambassador.java
+++ OOPInheritanceChallenge/src/com/kamathinc/Ambassador.java
@@ -10,4 +10,23 @@ public class Ambassador extends Car {
         super("Ambassador", 4, 4, 4, 3, false, 1);
         this.monthsUsed = monthsUsed;
     }
+
+    public void accelatate(int rate){
+        int newVelocity = getCurrentVelocity() + rate;
+        if(newVelocity == 0 ){
+            stop();
+        }else if(newVelocity > 0 && newVelocity <=10){
+            setCurrentGear(2);
+        }else if(newVelocity > 10 && newVelocity <=20){
+            setCurrentGear(3);
+        }else if(newVelocity > 20 && newVelocity <=45){
+            setCurrentGear(4);
+        }else{
+            setCurrentGear(5);
+        }
+
+        if (newVelocity > 0){
+            changeVelocity(newVelocity, getCurrentDirection());
+        }
+    }
 }
--- OOPInheritanceChallenge/src/com/kamathinc/Main.java
+++ OOPInheritanceChallenge/src/com/kamathinc/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Ambassador ambassador = new Ambassador(24);
+
+        ambassador.accelatate(30);
+        ambassador.steer(30);
+        ambassador.getCurrentDirection();
+        ambassador.stop();
     }
 }
