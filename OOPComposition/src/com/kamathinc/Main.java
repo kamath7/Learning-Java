55ec86c33d45c31510b598153ac290127c78ad31
kamath7 <adithyakamath96@gmail.com>
2022-02-23 01:24:09 +0530

    creating instances

--- OOPComposition/src/com/kamathinc/Main.java
+++ OOPComposition/src/com/kamathinc/Main.java
@@ -5,6 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+        Case myCase = new Case("GG Model","Logitech",260, new Dimensions(12,14,15));
+        Monitor monitor = new Monitor("22 inch good one","Dell",1080, new Resolution(1600, 900));
+        Motherboard motherboard = new Motherboard("PowerHouse xx","Gigabyte",4,16,"SomeGoodBios");
+
+        PC myPc = new PC(myCase, motherboard, monitor);
+
+        System.out.println();
+
 
     }
 }
