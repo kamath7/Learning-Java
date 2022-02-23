38fd023a7f78b769877ba734befea45d8c169283
kamath7 <adithyakamath96@gmail.com>
2022-02-24 01:04:33 +0530

    multiple movie classes

--- OOPPolymorphism/src/com/kamathinc/Main.java
+++ OOPPolymorphism/src/com/kamathinc/Main.java
@@ -23,6 +23,42 @@ class FightClub extends  Movie{
     }
 }
 
+class UsualSuspects extends Movie{
+    public UsualSuspects(){
+        super("Usual Suspects");
+    }
+    @Override
+    public String plot(){
+        return "Handicapped guy makes a fool of the police";
+    }
+}
+
+class Birbal extends Movie{
+    public Birbal(){
+        super("Birbal");
+    }
+    public String plot(){
+        return "Finding a killer who did a murder 8 years ago";
+    }
+}
+
+class BoneHunter extends Movie{
+    public BoneHunter(){
+        super("Bone Hunter");
+    }
+    public String plot(){
+        return "Serial killer on the loose who likes bones";
+    }
+}
+
+class ForgottenMovie extends  Movie{
+    public ForgottenMovie(){
+        super("Don't remember the name :( ");
+    }
+}
+
+
+
 
 
 public class Main {
