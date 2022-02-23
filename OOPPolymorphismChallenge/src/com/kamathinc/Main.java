0acb9f767bc6871abcab75ff05f85901f55d0bdd
kamath7 <adithyakamath96@gmail.com>
2022-02-24 01:40:38 +0530

    creating a base class

---
+++ OOPPolymorphismChallenge/src/com/kamathinc/Main.java
@@ -0,0 +1,52 @@
+package com.kamathinc;
+
+
+class Car{
+
+    private String name;
+    private String engine;
+    private int cylinders;
+    private int wheels;
+
+    public Car(String name, int cylinders) {
+        this.name = name;
+        this.engine = "V8 Turbomax";
+        this.cylinders = cylinders;
+        this.wheels = 4;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void startEngine(){
+        System.out.println("Ranodm car started");
+    }
+
+    public void accelerate(int speed){
+        System.out.println(" Random car accelerated by "+speed);
+    }
+    public void brake(){
+        System.out.println("Random car brakes applied");
+    }
+
+}
+
+class Ambassador extends Car{}
+class Padmini extends Car{}
+class Murcielage extends Car{}
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
