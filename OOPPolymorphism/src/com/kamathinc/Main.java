13801497e15ad8647f1a1678935666978963c4bb
kamath7 <adithyakamath96@gmail.com>
2022-02-24 00:59:27 +0530

    starting with polymorphism

---
+++ OOPPolymorphism/src/com/kamathinc/Main.java
@@ -0,0 +1,25 @@
+package com.kamathinc;
+
+
+class Movie{
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot(){
+        return "Undefined plot";
+    }
+}
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+    }
+}
