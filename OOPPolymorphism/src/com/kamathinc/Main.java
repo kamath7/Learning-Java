e2fe762c3d1065eba1fecab68de4acd8e4559c10
kamath7 <adithyakamath96@gmail.com>
2022-02-24 01:09:56 +0530

    returning a random movie

--- OOPPolymorphism/src/com/kamathinc/Main.java
+++ OOPPolymorphism/src/com/kamathinc/Main.java
@@ -1,72 +1,86 @@
 package com.kamathinc;
 
 
-class Movie{
+class Movie {
     private String name;
 
     public Movie(String name) {
         this.name = name;
     }
 
-    public String plot(){
+    public String plot() {
         return "Undefined plot";
     }
 }
 
-class FightClub extends  Movie{
-    public FightClub(){
+class FightClub extends Movie {
+    public FightClub() {
         super("Fight Club");
     }
 
-    public String plot(){
+    public String plot() {
         return "Two guys fight and make soap";
     }
 }
 
-class UsualSuspects extends Movie{
-    public UsualSuspects(){
+class UsualSuspects extends Movie {
+    public UsualSuspects() {
         super("Usual Suspects");
     }
+
     @Override
-    public String plot(){
+    public String plot() {
         return "Handicapped guy makes a fool of the police";
     }
 }
 
-class Birbal extends Movie{
-    public Birbal(){
+class Birbal extends Movie {
+    public Birbal() {
         super("Birbal");
     }
-    public String plot(){
+
+    public String plot() {
         return "Finding a killer who did a murder 8 years ago";
     }
 }
 
-class BoneHunter extends Movie{
-    public BoneHunter(){
+class BoneHunter extends Movie {
+    public BoneHunter() {
         super("Bone Hunter");
     }
-    public String plot(){
+
+    public String plot() {
         return "Serial killer on the loose who likes bones";
     }
 }
 
-class ForgottenMovie extends  Movie{
-    public ForgottenMovie(){
+class ForgottenMovie extends Movie {
+    public ForgottenMovie() {
         super("Don't remember the name :( ");
     }
 }
 
-
-
-
-
 public class Main {
-
-
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+    }
 
+    public static Movie giveMeAMovie() {
+        int randomNum = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated was " + randomNum);
+        switch (randomNum) {
+            case 1:
+                return new FightClub();
+            case 2:
+                return new UsualSuspects();
+            case 3:
+                return new Birbal();
+            case 4:
+                return new BoneHunter();
+            case 5:
+                return new ForgottenMovie();
+        }
 
+        return null;
     }
 }
