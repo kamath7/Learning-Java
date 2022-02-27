09d47022f51af565264347bd37646ca7cd9aa2cc
kamath7 <adithyakamath96@gmail.com>
2022-02-28 01:30:25 +0530

    starting with the arrays

---
+++ Arrays/src/com/kamathinc/Main.java
@@ -0,0 +1,16 @@
+package com.kamathinc;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int[] myArray = new int[10];
+        myArray[2] = 69;
+        System.out.println(myArray[2]);
+
+        double[] anotheArray = new double[10];
+        anotheArray[3] = 60;
+        System.out.println(anotheArray[3]);
+    }
+}
