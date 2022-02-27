a0f6a5ae83962f39c56dc1568c817a68a09caf37
kamath7 <adithyakamath96@gmail.com>
2022-02-28 01:47:25 +0530

    done adding

--- Arrays/src/com/kamathinc/Main.java
+++ Arrays/src/com/kamathinc/Main.java
@@ -1,6 +1,11 @@
 package com.kamathinc;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args) {
 	// write your code here
@@ -25,5 +30,39 @@ public class Main {
         for (int i = 0 ; i<coolArray.length ; i++){
             System.out.println(coolArray[i]);
         }
+
+        printMyArray(coolArray);
+
+        int[] someMoreNums = getNum(11);
+        printMyArray(someMoreNums);
+        System.out.println("average of the array is "+getAvg(someMoreNums));
+
+    }
+
+    public static int[] getNum(int number){
+        System.out.println("Enter "+number+" values");
+
+        int [] values = new int[number];
+
+        for (int i = 0 ; i < values.length ; i++){
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static void printMyArray(int[] arr){
+        for (int i = 0 ; i < arr.length ; i++){
+            System.out.println(i+" -> "+arr[i]);
+        }
+    }
+
+    public static float getAvg (int[] arr){
+        int sum = 0;
+
+        for(int i = 0 ; i < arr.length; i ++){
+            sum += arr[i];
+        }
+        return sum/(arr.length);
     }
 }
