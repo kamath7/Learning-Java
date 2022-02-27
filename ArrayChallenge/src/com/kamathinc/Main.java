f2c7648a29fb86f6f4f9f238ab84940b6589c1a5
kamath7 <adithyakamath96@gmail.com>
2022-02-28 01:56:10 +0530

    implementing methods

---
+++ ArrayChallenge/src/com/kamathinc/Main.java
@@ -0,0 +1,49 @@
+package com.kamathinc;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+	// write your code here
+
+        int[] myArr = getIntegers(6);
+
+    }
+
+    public static int[] getIntegers(int number){
+        System.out.println("Enter "+number+" values");
+
+        int []myArr = new int[number];
+
+        for (int i = 0 ; i < myArr.length; i++){
+            myArr[i] = scanner.nextInt();
+        }
+
+        return myArr;
+    }
+
+    public static void printArray(int[] arr){
+        for (int i =0 ; i < arr.length ; i++){
+            System.out.println(i+" -> "+arr[i]);
+        }
+    }
+
+    public static int[] sortMe(int[] arr){
+        for (int i = 0; i < arr.length; i++)
+        {
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                int tmp = 0;
+                if (arr[i] > arr[j])
+                {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+        return arr;
+    }
+}
