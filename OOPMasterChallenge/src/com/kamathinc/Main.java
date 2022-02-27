68df6cbce9164a9c84b318bc467d458367d9eed6
kamath7 <adithyakamath96@gmail.com>
2022-02-28 00:17:23 +0530

    testing hamburger with additions

--- OOPMasterChallenge/src/com/kamathinc/Main.java
+++ OOPMasterChallenge/src/com/kamathinc/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Hamburger hamburger = new Hamburger("McFish","Fish", 5.60, "Wheat bread");
+        System.out.println(hamburger.totalValue());
+
+        hamburger.addHamburderAddition1("Pickles",0.90);
+        hamburger.addHamburderAddition2("kethcup",0.60);
+
+        System.out.println(hamburger.totalValue());
     }
 }
