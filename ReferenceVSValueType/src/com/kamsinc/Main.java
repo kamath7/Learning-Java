36cca6ffcb9fa4434fe10ce3d0e9504baf45795f
kamath7 <adithyakamath96@gmail.com>
2022-03-21 13:56:43 +0530

    how reference works added

--- ReferenceVSValueType/src/com/kamsinc/Main.java
+++ ReferenceVSValueType/src/com/kamsinc/Main.java
@@ -1,5 +1,7 @@
 package com.kamsinc;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,12 +13,23 @@ public class Main {
         int someVal = 10;
         int someOtherVal = someVal;
 
-        System.out.println("Value1 -> "+someVal);
-        System.out.println("Value2 -> "+someOtherVal);
+        System.out.println("Value1 -> "+someVal);//10
+        System.out.println("Value2 -> "+someOtherVal);//10
 
         someVal++;
 
-        System.out.println("Value1 -> "+someVal);
-        System.out.println("Value2 -> "+someOtherVal);
+        System.out.println("Value1 -> "+someVal);//11
+        System.out.println("Value2 -> "+someOtherVal);//10
+
+        int []someIntArr = new int[5];
+        int []someOtherIntArr = someIntArr; //referring to the same object. Kind of like bruce wayne's house points to batman and bruce wayne
+
+        System.out.println("Array1 -> "+ Arrays.toString(someIntArr));
+        System.out.println("Array2 -> "+ Arrays.toString(someOtherIntArr));
+
+        someIntArr[0] = 69;
+
+        System.out.println("Array1 -> "+ Arrays.toString(someIntArr));
+        System.out.println("Array2 -> "+ Arrays.toString(someOtherIntArr));
     }
 }
