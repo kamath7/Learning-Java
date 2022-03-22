9140fd4ccd8ed4c0f470932d7357c51765467c87
kamath7 <adithyakamath96@gmail.com>
2022-03-22 14:42:05 +0530

    refactoring

--- ArrayList1/src/com/kamsinc/Main.java
+++ ArrayList1/src/com/kamsinc/Main.java
@@ -6,15 +6,8 @@ import java.util.Scanner;
 public class Main {
     final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-	// write your code hereint
-        int[] myArr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        //resizing
-        int[] originalArr = myArr;
-        System.out.println("Original Arr ->" + Arrays.toString(myArr));
-        myArr = new int[12];
-        for(int i = 0 ; i < originalArr.length; i++){
-            myArr[i] = originalArr[i];
-        }
-        System.out.println("Resized Arr ->" + Arrays.toString(myArr));
+        //List is an interface
+        //ArrayList - handles size internally. no need to mention
+
     }
 }
