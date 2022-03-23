12edede58beafa3cba3fcf0e85ba9132055d4998
kamath7 <adithyakamath96@gmail.com>
2022-03-24 00:13:59 +0530

    added update contact

--- ArrayListChallenge/src/com/kamsinc/MobilePhone.java
+++ ArrayListChallenge/src/com/kamsinc/MobilePhone.java
@@ -14,6 +14,17 @@ public class MobilePhone {
         myContacts.add(contact);
         return true;
     }
+
+    public boolean updateContact(Contact oldContact, Contact newContact){
+        int ifFound= findContact(oldContact);
+        if (ifFound < 0){
+            System.out.println("Contact doesn't exist to update!");
+            return false;
+        }
+        this.myContacts.set(ifFound, newContact);
+        System.out.println(oldContact.getName()+" changed to "+newContact.getName()+" "+newContact.getPhoneNumber());
+        return true;
+    }
     private int findContact(Contact contact){
         return this.myContacts.indexOf(contact);
     }
