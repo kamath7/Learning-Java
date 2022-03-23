8278057fca3658d12574bfc7c030e34e07e1506d
kamath7 <adithyakamath96@gmail.com>
2022-03-24 00:11:14 +0530

    checking for duplicate contacts

--- ArrayListChallenge/src/com/kamsinc/Contacts.java
+++
@@ -1,6 +0,0 @@
-package com.kamsinc;
-
-public class Contacts {
-    private String name;
-    private String phoneNumber;
-}
--- ArrayListChallenge/src/com/kamsinc/MobilePhone.java
+++ ArrayListChallenge/src/com/kamsinc/MobilePhone.java
@@ -1,5 +1,29 @@
 package com.kamsinc;
 
-public class MobilePhone {
+import java.util.ArrayList;
 
+public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public boolean addContact (Contact contact){
+        if (findContact(contact.getName()) > 0){
+            System.out.println("Contact already exists!");
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+    private int findContact(Contact contact){
+        return this.myContacts.indexOf(contact);
+    }
+    private int findContact(String name){ //Overloading
+        for(int i = 0 ; i < this.myContacts.size(); i++){
+            Contact contact = this.myContacts.get(i);
+            if (contact.getName().equals(name)){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
