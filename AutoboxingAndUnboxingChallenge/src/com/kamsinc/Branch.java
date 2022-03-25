ca1cb00cdb862f8c56201753e392a7a835c652e8
kamath7 <adithyakamath96@gmail.com>
2022-03-25 15:41:44 +0530

    setting up branch class

--- AutoboxingAndUnboxingChallenge/src/com/kamsinc/Branch.java
+++ AutoboxingAndUnboxingChallenge/src/com/kamsinc/Branch.java
@@ -1,4 +1,18 @@
 package com.kamsinc;
 
+import java.util.ArrayList;
+
 public class Branch {
+    private String branchName;
+    private ArrayList<Customer> customers;
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public Branch(String branchName) {
+        this.branchName = branchName;
+        this.customers = new ArrayList<Customer>();
+
+    }
 }
