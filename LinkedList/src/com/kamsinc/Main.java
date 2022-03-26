99ad30ad76321ea5445875d3d5a9ca62a964969e
kamath7 <adithyakamath96@gmail.com>
2022-03-27 01:52:10 +0530

    setting up linkedlist

--- LinkedList/src/com/kamsinc/Main.java
+++ LinkedList/src/com/kamsinc/Main.java
@@ -1,5 +1,6 @@
 package com.kamsinc;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Main {
@@ -14,5 +15,19 @@ public class Main {
 
         System.out.println("Balance of Cust1 -> "+customer.getBalance());
         System.out.println("Balance of Cust2 ->"+customer1.getBalance());
+
+        ArrayList<Integer> myIntArr = new ArrayList<>();
+        myIntArr.add(69);
+        myIntArr.add(138);
+        myIntArr.add(69);
+
+        for(int i =0; i < myIntArr.size(); i++){
+            System.out.println(i+" -> "+myIntArr.get(i));
+        }
+        myIntArr.add(1,35);
+
+        for(int i =0; i < myIntArr.size(); i++){
+            System.out.println(i+" -> "+myIntArr.get(i));
+        }
     }
 }
---
+++ LinkedList/src/com/kamsinc/MyLL.java
@@ -0,0 +1,21 @@
+package com.kamsinc;
+
+import java.util.LinkedList;
+
+public class MyLL {
+
+    public static void main(String[] args) {
+        LinkedList<String> somePlaces = new LinkedList<>();
+
+        somePlaces.add("Hampi");
+        somePlaces.add("Mangalore");
+        somePlaces.add("Gokarna");
+        somePlaces.add("Rio De Janeiro");
+
+        printMyList(somePlaces);
+    }
+
+    private static void printMyList(LinkedList<String> places){
+
+    }
+}
