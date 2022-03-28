5aea1c12cdb0dead9c9aad7a51cbacb86ed38508
kamath7 <adithyakamath96@gmail.com>
2022-03-29 00:25:11 +0530

    adding to playlist

--- LinkedListChallenge/src/com/kamsinc/Album.java
+++ LinkedListChallenge/src/com/kamsinc/Album.java
@@ -1,6 +1,7 @@
 package com.kamsinc;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Album {
     private String name;
@@ -28,6 +29,26 @@ public class Album {
             }
         }
         return null;
+    }
+
+    public boolean addToPlaylist(int trackNo, LinkedList<Song> playList){
+        int index = trackNo - 1;
+        if(index > 0 && (index <=this.songs.size())){
+            playList.add(this.songs.get(index));
+            return true;
+        }
+        System.out.println("Album currently empty");
+        return false;
+    }
+
+    public boolean addToPlaylist(String title, LinkedList<Song> playList){
+        Song checkSong = findSong(title);
+        if (checkSong !=null){
+            playList.add(checkSong);
+            return  true;
+        }
+        System.out.println("Song not present in the album");
+        return  false;
     }
 
 }
