3251fc70c05bdc33bb2365b85642f62b8d756a75
kamath7 <adithyakamath96@gmail.com>
2022-03-30 00:05:45 +0530

    implemented methods

--- Interfaces/src/com/kamsinc/DeskPhone.java
+++ Interfaces/src/com/kamsinc/DeskPhone.java
@@ -2,31 +2,44 @@ package com.kamsinc;
 
 public class DeskPhone implements ITelephone {
 
-    private int phoneNumber;
+    private int myPhoneNumber;
     private boolean phoneRinging;
 
     @Override
     public void powerOn() {
-
+        System.out.println("Phone is always present. Phone always on!");
     }
 
     @Override
     public void dial(int phoneNumber) {
-
+        System.out.println("Phone now ringing. Call by "+phoneNumber);
     }
 
     @Override
     public void answer() {
-
+        if (phoneRinging){
+            System.out.println("Phone being answered now!");
+            this.phoneRinging = false;
+        }
     }
 
     @Override
     public boolean callPhone(int phoneNumber) {
+        if(phoneNumber == myPhoneNumber){
+            phoneRinging = true;
+            System.out.println("TRING TRING");
+        }else{
+            phoneRinging = false;
+        }
         return false;
     }
 
     @Override
     public boolean phoneRinging() {
-        return false;
+        return this.phoneRinging;
+    }
+
+    public DeskPhone(int myPhoneNumber) {
+        this.myPhoneNumber = myPhoneNumber;
     }
 }
--- Interfaces/src/com/kamsinc/Main.java
+++ Interfaces/src/com/kamsinc/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         // Methods are specified that a particular class must implement - Interface
+        ITelephone somePhone;
+        somePhone = new DeskPhone(7787290);
+        somePhone.powerOn();
+        somePhone.callPhone(1289099);
+        somePhone.answer();
 
     }
 }
