e4f12d0038e9ee5b83ad67c0651e2513b619a4e0
kamath7 <adithyakamath96@gmail.com>
2022-03-29 23:55:57 +0530

    defining an interface

---
+++ Interfaces/src/com/kamsinc/ITelephone.java
@@ -0,0 +1,11 @@
+package com.kamsinc;
+
+public interface ITelephone {
+
+    public void powerOn();
+    public void dial(int phoneNumber);
+    public void answer();
+    public boolean callPhone(int phoneNumber);
+    public boolean phoneRinging();
+
+}
---
+++ Interfaces/src/com/kamsinc/Main.java
@@ -0,0 +1,10 @@
+package com.kamsinc;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // Methods are specified that a particular class must implement - Interface
+
+    }
+}
