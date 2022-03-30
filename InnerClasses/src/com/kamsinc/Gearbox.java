b2abd91e6bc3d1bde98e2c57c24099723aa03b9c
kamath7 <adithyakamath96@gmail.com>
2022-03-30 14:51:40 +0530

    making an inner class private

--- InnerClasses/src/com/kamsinc/Gearbox.java
+++ InnerClasses/src/com/kamsinc/Gearbox.java
@@ -7,6 +7,7 @@ public class Gearbox {
     private ArrayList<Gear> gears;
     private int maxGears;
     private int currentGear = 0;
+    private boolean clutchIn;
 
     public Gearbox(int maxGears) {
         this.maxGears = maxGears;
@@ -15,9 +16,38 @@ public class Gearbox {
         this.gears.add(neutral);
     }
 
-    public class Gear{
+    public void operateClutch(boolean in){
+        this.clutchIn = in;
+    }
+
+    public void addGear(int number, double ratio){
+        if( number > 0 && number <= maxGears){
+            this.gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int newGear){
+        if (newGear >=0 && newGear < this.gears.size() && this.clutchIn){
+            this.currentGear = newGear;
+            System.out.println(" Gear - "+newGear+" in place");
+        }
+        System.out.println("FTFY");
+        this.currentGear = 0;
+    }
+    public double changeWheelSpeed(int revs){
+        if(clutchIn){
+            System.out.println("BOOOOOM");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
+    }
+    private class Gear{
         private int gearNumber;
         private double ratio;
+
+        public double getRatio() {
+            return ratio;
+        }
 
         public Gear(int gearNumber, double ratio) {
             this.gearNumber = gearNumber;
