c4d993003950d0c88db1a7b787147596081a94e4
kamath7 <adithyakamath96@gmail.com>
2022-03-30 22:43:25 +0530

    changes to gear

--- InnerClasses/src/com/kamsinc/Main.java
+++ InnerClasses/src/com/kamsinc/Main.java
@@ -6,6 +6,14 @@ public class Main {
 	// write your code here
 
         Gearbox ferrari = new Gearbox(7);
+
+        ferrari.operateClutch(true);
+        ferrari.changeGear(1);
+        ferrari.operateClutch(false);
+        System.out.println(ferrari.changeWheelSpeed(900));
+        ferrari.changeGear(2);
+        System.out.println(ferrari.changeWheelSpeed(1000));
+        ferrari.changeGear(3);
 //        Gearbox.Gear first = ferrari.new Gear(1,12.3);
 //        Gearbox.Gear second = ferrari.new Gear(2, 23.4);
 //        Gearbox.Gear third = ferrari.new Gear(3, 27.2);
