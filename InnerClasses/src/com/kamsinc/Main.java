597a23a3c8c84543836f8ccaf724d495e7d4078a
kamath7 <adithyakamath96@gmail.com>
2022-03-30 14:46:10 +0530

    adding more gears

--- InnerClasses/src/com/kamsinc/Main.java
+++ InnerClasses/src/com/kamsinc/Main.java
@@ -7,7 +7,11 @@ public class Main {
 
         Gearbox ferrari = new Gearbox(7);
         Gearbox.Gear first = ferrari.new Gear(1,12.3);
+        Gearbox.Gear second = ferrari.new Gear(2, 23.4);
+        Gearbox.Gear third = ferrari.new Gear(3, 27.2);
 
         System.out.println(first.driveSpeed(1000));
+        System.out.println(second.driveSpeed(900));
+        System.out.println(third.driveSpeed(280));
     }
 }
