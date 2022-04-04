14c313847ec52dab938692d9f08b6592d44e5656
kamath7 <adithyakamath96@gmail.com>
2022-04-05 00:15:27 +0530

    starting with BST

---
+++ AbstractClassesChallenge/src/com/kamsinc/SearchTree.java
@@ -0,0 +1,31 @@
+package com.kamsinc;
+
+//Implementing a BST
+public class SearchTree implements NodeList{
+
+    private ListItem root = null;
+
+    public SearchTree(ListItem root){
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+
+    }
+}
