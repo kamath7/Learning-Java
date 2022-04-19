3b5d576e974c5e9d02aaf154a3798d81e51eaae6
kamath7 <adithyakamath96@gmail.com>
2022-04-20 01:54:24 +0530

    cricket team

--- GenericClass/src/Main.java
+++ GenericClass/src/Main.java
@@ -6,8 +6,9 @@ public class Main {
 
 
     Team<FootballPlayer> lalleTeam = new Team<>("Awesome Team");
-
+    Team <CricketPlayer> lalleTeam2 = new Team<>("CSK");
     lalleTeam.addPlayer(max);
+    lalleTeam2.addPlayer(dhuni);
 //    lalleTeam.addPlayer(dhuni); -> Invalid now
 
         System.out.println(lalleTeam.playerCount());
