d1988ae6d7090d2b4b6284942c17c23eacc6dc5c
kamath7 <adithyakamath96@gmail.com>
2022-04-20 12:58:52 +0530

    implement comparable

--- GenericClass/src/Main.java
+++ GenericClass/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -8,6 +10,8 @@ public class Main {
 
     Team<FootballPlayer> lalleTeam = new Team<>("Awesome Team");
     Team <CricketPlayer> lalleTeam2 = new Team<>("CSK");
+    Team<FootballPlayer> lalleTeam4 = new Team<>("Awesome Team2");
+
 
     Team<BaseballPlayer> lalleTeam3 = new Team<>("Shitty team");
     lalleTeam3.addPlayer(someDude);
@@ -15,9 +19,16 @@ public class Main {
     lalleTeam2.addPlayer(dhuni);
 //    lalleTeam.addPlayer(dhuni); -> Invalid now
 //
-    System.out.println(lalleTeam.playerCount());
+    System.out.println(lalleTeam2.playerCount());
 
+    lalleTeam.matchResult(lalleTeam4,1,2);
 
+        System.out.println("Rankings");
+        System.out.println(lalleTeam.getName()+" "+lalleTeam.ranking());
+
+        System.out.println(lalleTeam.compareTo(lalleTeam4));
+
+        ArrayList<Team> teams;
 
     }
 }
--- GenericClass/src/Team.java
+++ GenericClass/src/Team.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
-public class Team<T extends Player> {
+public class Team<T extends Player> implements Comparable<Team<T>> {
+    //Team<T> added since we can't compare football to cricket team
     private String name;
     int played = 0;
     int won = 0 ;
@@ -54,5 +55,17 @@ public class Team<T extends Player> {
 
     public int ranking(){
         return (won * 2) + tied;
+    }
+
+    @Override
+    public int compareTo(Team<T> team) {
+        if(this.ranking() > team.ranking()){
+            return -1;
+        }else if(this.ranking() < team.ranking()){
+            return 1;
+        }else{
+            return 0;
+        }
+
     }
 }
