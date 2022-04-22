0fba602b7d67e5ad5f45d3c66cc13dab9a18b1df
kamath7 <adithyakamath96@gmail.com>
2022-04-23 02:20:00 +0530

    most shitty way of showing scope

---
+++ ScopeChallenge/src/S.java
@@ -0,0 +1,13 @@
+public class S {
+    private int s;
+
+    public S(int s) {
+        this.s = s;
+    }
+
+    public void s(){
+        for (int s =1 ; s< 13; s++){
+            System.out.println(s +" * "+ this.s+" -> "+(s * this.s));
+        }
+    }
+}
