872d601e46dbe226abd1843ceee4a15cc22bf489
kamath7 <adithyakamath96@gmail.com>
2022-05-31 00:24:37 +0530

    more testing

--- FinalKeyword/src/Main.java
+++ FinalKeyword/src/Main.java
@@ -9,7 +9,15 @@ public class Main {
         System.out.println(Math.PI);
 //      Math m = new Math(); - won't happen. constructor is marked private since you shouldn't be creating instances of this class
 
+
         EliteClass eliteClass  = new EliteClass(123450);
         eliteClass.storePass();
+        int correctPass = 123450;
+        int wrongPass = 1239;
+
+        System.out.println(eliteClass.letMeIn(correctPass));
+        System.out.println(eliteClass.letMeIn(wrongPass));
+
+
     }
 }
