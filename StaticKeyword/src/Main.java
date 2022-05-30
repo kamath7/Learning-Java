f4363bde8b64f64b22f44fe77490cf418ab923b6
kamath7 <adithyakamath96@gmail.com>
2022-05-30 19:43:49 +0530

    usage of sms

--- StaticKeyword/src/Main.java
+++ StaticKeyword/src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
+
+    public static int catalyst = 69;
     public static void main(String[] args) {
 
         StaticTst staticTst = new StaticTst("Joker");
@@ -7,5 +9,12 @@ public class Main {
         StaticTst staticTst1 = new StaticTst("Gateshead");
         System.out.println(staticTst1.getName()+". Instance num - "+StaticTst.getNumInstances());
 
+        int ans = catalyse(69); //won't work without the static keyword in catalyse method
+        System.out.println(ans);
+
+    }
+
+    public static int catalyse(int num){
+        return num * catalyst;
     }
 }
