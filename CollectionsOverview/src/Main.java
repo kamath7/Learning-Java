e00c078b0a4f419adb387059f5ec240b08ee29e9
kamath7 <adithyakamath96@gmail.com>
2022-05-31 14:09:38 +0530

    theatre class

---
+++ CollectionsOverview/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Sets map, queues - Part of collections
+
+
+
+    }
+}
---
+++ CollectionsOverview/src/Theatre.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Theatre {
+
+    private final String theatreName;
+    private List<Seat> seats = new ArrayList<>();
+
+    public Theatre(String theatreName, int numRows, int seatsPerRow) {
+        this.theatreName = theatreName;
+        int lastRow = 'A' + (numRows -1);
+        for (char row = 'A'; row <= lastRow; row++){
+            for (int seatNum =1 ; seatNum <= seatsPerRow ; seatNum ++){
+                Seat seat = new Seat(row + String.format("%02d", seatNum));
+                seats.add(seat);
+            }
+        }
+    }
+
+    public String getTheatreName() {
+        return theatreName;
+    }
+}
