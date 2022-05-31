d932669010cf15f0318faf7433059cd125db9b76
kamath7 <adithyakamath96@gmail.com>
2022-05-31 14:20:30 +0530

    Testing the theatre

--- CollectionsOverview/src/Main.java
+++ CollectionsOverview/src/Main.java
@@ -3,7 +3,20 @@ public class Main {
 
         //Sets map, queues - Part of collections
 
+        Theatre theatre = new Theatre("Jyothi Talkies", 20, 5); //it doesn't exist anymore ☹
+        theatre.getSeats();
 
+        if(theatre.reserveSeat("A02")){
+            System.out.println("PAY ME CASH!");
+        }else{
+            System.out.println("Seat reserved");
+        }
 
+        //below won't work
+//        if(theatre.reserveSeat("A02")){
+//            System.out.println("PAY ME CASH!");
+//        }else{
+//            System.out.println("Seat reserved");
+//        }
     }
 }
