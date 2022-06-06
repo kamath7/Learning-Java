0ca8d98301b062f2f65402e2c13478a1d9c1e827
kamath7 <adithyakamath96@gmail.com>
2022-06-07 00:50:37 +0530

    testing planets

--- SetsAndHashSet/src/Main.java
+++ SetsAndHashSet/src/Main.java
@@ -1,9 +1,34 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
+
+    private  static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Set<HeavenlyBody> plantets = new HashSet<>();
     public static void main(String[] args) {
 
         //sets are unordered
+        HeavenlyBody temp = new HeavenlyBody("Mercury",22);
+        solarSystem.put(temp.getName(), temp);
+        plantets.add(temp);
 
+        temp = new HeavenlyBody("Venus",53);
+        solarSystem.put(temp.getName(), temp);
+        plantets.add(temp);
 
+        temp = new HeavenlyBody("Earth",87);
+        solarSystem.put(temp.getName(), temp);
+        plantets.add(temp);
+
+        HeavenlyBody tempMoon = new HeavenlyBody("Moon",210);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+
+        System.out.println("Planets ");
+        for(HeavenlyBody planet: plantets){
+            System.out.println("\n"+ planet.getName());
+        }
 
 
     }
