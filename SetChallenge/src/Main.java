0ba42fb1af0ca10f5778f197a3b9fc5e304c1ad5
kamath7 <adithyakamath96@gmail.com>
2022-06-08 20:41:51 +0530

    printing out the solar system

--- SetChallenge/src/Main.java
+++ SetChallenge/src/Main.java
@@ -26,40 +26,40 @@ public class Main {
         solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
-        HeavenlyBody temp1 = new Planet("Mars",190);
+        HeavenlyBody temp1 = new Planet("Mars", 190);
 
         HeavenlyBody tempMoon = new Moon("Moon", 29);
         solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon);
 
         System.out.println("Planets ");
-        for(HeavenlyBody planet: planets){
-            System.out.println("\n"+ planet.getKey());
+        for (HeavenlyBody planet : planets) {
+            System.out.println("\n" + planet.getKey());
         }
 
         HeavenlyBody body = solarSystem.get(HeavenlyBody.makeKey("Earth", HeavenlyBody.BodyTypes.PLANET));
-        System.out.println("Moons of body "+body.getKey());
-        for(HeavenlyBody moons: body.getSatelittes()){
-            System.out.println("\n"+moons.getKey());
+        System.out.println("Moons of body " + body.getKey());
+        for (HeavenlyBody moons : body.getSatelittes()) {
+            System.out.println("\n" + moons.getKey());
         }
 
         Set<HeavenlyBody> moons = new HashSet<>();
-        for (HeavenlyBody planet: planets){
+        for (HeavenlyBody planet : planets) {
             moons.addAll(planet.getSatelittes());
         }
         System.out.println("All moons");
-        for(HeavenlyBody moon: moons){
-            System.out.println("\n"+moon.getKey());
+        for (HeavenlyBody moon : moons) {
+            System.out.println("\n" + moon.getKey());
         }
 
 
-        for(HeavenlyBody planet: planets){
+        for (HeavenlyBody planet : planets) {
 //            System.out.println(planet.getKey() + " -> "+planet.getOrbitalPeriod());
             System.out.println(planet);
         }
 
-        HeavenlyBody earth1 = new Planet("Earth",21);
-        HeavenlyBody earth2 = new Planet("Earth",211);
+        HeavenlyBody earth1 = new Planet("Earth", 21);
+        HeavenlyBody earth2 = new Planet("Earth", 211);
         System.out.println(earth2.equals(earth1));
         System.out.println(earth1.equals(earth2));
         System.out.println(earth1.equals(temp1));
@@ -69,7 +69,11 @@ public class Main {
         System.out.println(solarSystem.get(HeavenlyBody.makeKey("Earth", HeavenlyBody.BodyTypes.PLANET)));
         System.out.println(solarSystem.get(HeavenlyBody.makeKey("Mars", HeavenlyBody.BodyTypes.PLANET)));
 
+        System.out.println("Solar System");
 
+        for (HeavenlyBody heavenlyBody : solarSystem.values()) {
+            System.out.println(heavenlyBody);
+        }
 
     }
 }
