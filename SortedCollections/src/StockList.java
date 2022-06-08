82fa3f661acacdbd7826e637ee328ef32d95eed7
kamath7 <adithyakamath96@gmail.com>
2022-06-09 02:36:32 +0530

    Returning list of stock items

--- SortedCollections/src/StockList.java
+++ SortedCollections/src/StockList.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -6,7 +7,7 @@ public class StockList {
     private final Map<String, StockItem> list;
 
     public StockList() {
-        this.list = new HashMap<>();
+        this.list = new HashMap<String, StockItem>();
     }
 
     public int addStock(StockItem item){
@@ -20,5 +21,27 @@ public class StockList {
             return item.getQuantityStock();
         }
         return 0; //no stonks for you
+    }
+
+    public int sellStock(String item, int quantity){
+        StockItem inStock = list.getOrDefault(item, null);
+
+        if((inStock != null) && (inStock.getQuantityStock() >= quantity) && (quantity > 0)){
+            inStock.adjuststock(-quantity);
+            return quantity;
+        }
+        return 0;
+    }
+
+    public Map<String, StockItem> getList() {
+        return list;
+    }
+
+    public StockItem get(String key){
+        return this.get(key);
+    }
+
+    public Map<String, StockItem> Items(){
+        return Collections.unmodifiableMap(list);
     }
 }
