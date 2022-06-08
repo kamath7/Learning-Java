ff781d87fff93baef07970a1511fc30526e6f78e
kamath7 <adithyakamath96@gmail.com>
2022-06-08 20:09:02 +0530

    testing

--- SetChallenge/src/Main.java
+++ SetChallenge/src/Main.java
@@ -1,5 +1,66 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
+
+    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Set<HeavenlyBody> planets = new HashSet<>();
+
     public static void main(String[] args) {
+
+        HeavenlyBody temp = new Planet("Earth", 90);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new Planet("Mars", 200);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new Planet("Venus", 910);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new DwarfPlanet("Pluto", 12);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        HeavenlyBody tempMoon = new Moon("Moon", 29);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addSatellite(tempMoon);
+
+        System.out.println("Planets ");
+        for(HeavenlyBody planet: planets){
+            System.out.println("\n"+ planet.getName());
+        }
+
+        HeavenlyBody body = solarSystem.get("Earth");
+        System.out.println("Moons of body "+body.getName());
+        for(HeavenlyBody moons: body.getSatelittes()){
+            System.out.println("\n"+moons.getName());
+        }
+
+        Set<HeavenlyBody> moons = new HashSet<>();
+        for (HeavenlyBody planet: planets){
+            moons.addAll(planet.getSatelittes());
+        }
+        System.out.println("All moons");
+        for(HeavenlyBody moon: moons){
+            System.out.println("\n"+moon.getName());
+        }
+
+
+        for(HeavenlyBody planet: planets){
+//            System.out.println(planet.getName() + " -> "+planet.getOrbitalPeriod());
+            System.out.println(planet);
+
+        }
+
+        Object o = new Object(); //base class
+
+        o.equals(o);
+        System.out.println(("lalle".equals("")));
 
 
     }
