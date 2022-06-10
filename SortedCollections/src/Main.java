0ef1d64f786a58785ecb1541c634d3219433fedf
kamath7 <adithyakamath96@gmail.com>
2022-06-10 20:20:04 +0530

    clear basket

--- SortedCollections/src/Main.java
+++ SortedCollections/src/Main.java
@@ -75,4 +75,11 @@ public class Main {
         }
         return 0;
     }
+
+    public static void checkout(Basket basket){
+        for (Map.Entry<StockItem,Integer> item: basket.Items().entrySet()){
+            stockList.sellStock(item.getKey().getName() , item.getValue());
+        }
+        basket.clearBasket();
+    }
 }
