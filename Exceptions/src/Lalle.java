1452533fe40172d01af0db61a5405f41a202d4e3
kamath7 <adithyakamath96@gmail.com>
2022-07-11 22:30:40 +0530

    printing stack trace

--- Exceptions/src/Lalle.java
+++ Exceptions/src/Lalle.java
@@ -2,19 +2,27 @@ import java.util.Scanner;
 
 public class Lalle {
     final static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         int res = divide();
         System.out.println(res);
     }
 
-    private static int divide(){
+    private static int divide() {
         int x = getNum();
         int y = getNum();
-        System.out.println("x - >" +x + " y-> "+y);
-        return x/y;
+        System.out.println("x - >" + x + " y-> " + y);
+        try {
+            return x / y;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
-    private static int getNum(){
+
+    private static int getNum() {
         System.out.println("Enter num");
-        return  scanner.nextInt();
+        return scanner.nextInt();
     }
 }
