be93b84c985b9a039716ab3c7f92afec6b78ff9e
kamath7 <adithyakamath96@gmail.com>
2022-07-11 21:06:03 +0530

    validating input

--- Exceptions/src/Main.java
+++ Exceptions/src/Main.java
@@ -1,13 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        int x = 10;
-        int y= 0;
+        int x = validatedInput();
+        int y= validatedInput();
 
         System.out.println(divideByANum(x,y));
         System.out.println(divideByANumAgain(x,y));
+        System.out.println(iWillCrashDivide(x,y));
     }
 
+    private static int exceptioninput(){
+
+    }
+
+    private static int validatedInput (){
+        boolean isValid = true;
+        System.out.println("Enter num");
+        String ip = scanner.next();
+        for(int i =0 ; i < ip.length(); i++){
+            if (!Character.isDigit(ip.charAt(i))){
+                isValid = false;
+                break;
+            }
+        }
+        if(isValid){
+            return Integer.parseInt(ip);
+        }
+        return 0;
+    }
     public static int divideByANum(int x, int y){
         if (y != 0){
             return x / y ;
@@ -22,5 +45,8 @@ public class Main {
             System.out.println(e);
         }
         return 0;
+    }
+    private static int iWillCrashDivide(int x, int y){
+        return x/y;
     }
 }
