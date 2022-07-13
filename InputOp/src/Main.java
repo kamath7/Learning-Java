3cfed49e4510c8c9e75a068b3c8a701e1f2c5b87
kamath7 <adithyakamath96@gmail.com>
2022-07-14 00:17:36 +0530

    static initialisation block

--- InputOp/src/Main.java
+++ InputOp/src/Main.java
@@ -3,9 +3,10 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    private static Locations locations = new Locations();
+
     public static void main(String[] args) {
         //Done usually using byte or character data
-
         Scanner scanner = new Scanner(System.in);
 
         Map<String, String> vocabulary = new HashMap<String, String>();
