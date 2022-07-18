1c8fe1ceea01ce8c9dc6df0f80ac78436223c2f6
kamath7 <adithyakamath96@gmail.com>
2022-07-19 00:34:14 +0530

    replacing with bufferedReader

--- InputOp/src/Locations.java
+++ InputOp/src/Locations.java
@@ -23,7 +23,9 @@ public class Locations implements Map<Integer, Location> {
     }
 
     static {
-        try (Scanner scanner = new Scanner(new FileReader("locations_big.txt"))) {
+
+
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("locations_big.txt")))) {
             scanner.useDelimiter(",");
             while (scanner.hasNextLine()) {
                 int loc = scanner.nextInt();
