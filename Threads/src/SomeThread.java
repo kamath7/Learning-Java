7d15b1e50d85d62b1e593e2bfff705717d0d8919
kamath7 <adithyakamath96@gmail.com>
2022-07-22 01:49:41 +0530

    making the thread sleep

--- Threads/src/SomeThread.java
+++ Threads/src/SomeThread.java
@@ -1,8 +1,14 @@
 
-public class SomeThread extends Thread{
+public class SomeThread extends Thread {
     @Override
     public void run() {
-        System.out.println(ThreadColor.ANSI_BLUE+currentThread().getName()+" calling!");
-
+        System.out.println(ThreadColor.ANSI_BLUE + currentThread().getName() + " calling!");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println(ThreadColor.ANSI_BLUE+"Another one woke me ");
+            e.printStackTrace();
+        }
+        System.out.println(ThreadColor.ANSI_BLUE+"Awoken from 3 second slumber ");
     }
 }
