b609dda688d7a7acd8f44317667044597dc63b91
kamath7 <adithyakamath96@gmail.com>
2022-07-25 00:07:20 +0530

    adding break

--- MultipleThreads/src/Main.java
+++ MultipleThreads/src/Main.java
@@ -1,25 +1,38 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        CountDown countDown = new CountDown();
+        CountDownThread t1 = new CountDownThread(countDown);
+        t1.setName("Thread 1");
+        CountDownThread t2 = new CountDownThread(countDown);
+        t2.setName("Thread 2");
+        CountDownThread t3 = new CountDownThread(countDown);
+        t3.setName("Thread 3");
+        t1.start();
+        t2.start();
+        t3.start();
     }
 }
 
-class CountDown{
-    public void doACountdown(){
+class CountDown {
+    public void doACountdown() {
         String color;
-        switch(Thread.currentThread().getName()){
+        switch (Thread.currentThread().getName()) {
             case "Thread 1":
                 color = ThreadColor.ANSI_RED;
+                break;
             case "Thread 2":
-                color = ThreadColor.ANSI_BLACK;
+                color = ThreadColor.ANSI_GREEN;
+                break;
             case "Thread 3":
                 color = ThreadColor.ANSI_CYAN;
-            default:color = ThreadColor.ANSI_PURPLE;
+                break;
+            default:
+                color = ThreadColor.ANSI_PURPLE;
         }
 
-        for (int i = 10 ; i > 0 ; i--){
-            System.out.println(color+ Thread.currentThread().getName() + ": "+i);
+        for (int i = 10; i > 0; i--) {
+            System.out.println(color + Thread.currentThread().getName() + ": " + i);
         }
     }
 }
@@ -27,7 +40,7 @@ class CountDown{
 class CountDownThread extends Thread {
     private CountDown threadCountDown;
 
-    public CountDownThread(CountDown countDown){
+    public CountDownThread(CountDown countDown) {
         threadCountDown = countDown;
     }
 
