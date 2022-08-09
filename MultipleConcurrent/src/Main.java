39ed06b167a85b929c75eaeccbfb896c74e74081
kamath7 <adithyakamath96@gmail.com>
2022-08-09 20:39:00 +0530

    creating anon threads

--- MultipleConcurrent/src/Main.java
+++ MultipleConcurrent/src/Main.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Main {
+    public static final String EOF = "EOF";
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+     List<String> buffer = new ArrayList<String>();
+     MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_YELLOW);
+     Consumer consumer = new Consumer(buffer, ThreadColor.ANSI_CYAN);
+     Consumer consumer1 = new Consumer(buffer, ThreadColor.ANSI_WHITE);
+
+     new Thread(producer).start();
+     new Thread(consumer).start();
+     new Thread(consumer1).start();
     }
 }
 
