1c35d4c98152b1cf1aa6fb9d7d9e8049cdec364b
kamath7 <adithyakamath96@gmail.com>
2022-09-12 20:42:19 +0530

    changes to avoid deadlock temporarily

--- DeadLock/src/Main.java
+++ DeadLock/src/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        new Thread1().start();
+        new Thread2().start();
     }
     private static class Thread1 extends  Thread {
         @Override
@@ -32,20 +32,20 @@ public class Main {
     private  static class Thread2 extends  Thread {
         @Override
         public void run() {
-            synchronized (lock2){
-                System.out.println("Thread 2 has lock2 enabled");
+            synchronized (lock1){
+                System.out.println("Thread 2 has lock1 enabled");
                 try{
                     Thread.sleep(100);
                 }catch (InterruptedException e){
                     e.printStackTrace();
                 }
-                System.out.println("Waiting for lock: Thread 2");
-                synchronized (lock1){
+                System.out.println("Waiting for lock2: Thread 2");
+                synchronized (lock2){
                     System.out.println("Thread2: has lock1 and lock2 both enabled");
                 }
-                System.out.println("Thread2: Released lock1");
+                System.out.println("Thread2: Released lock2");
             }
-            System.out.println("Thread2: Release lock2. Exiting!");
+            System.out.println("Thread2: Release lock1. Exiting!");
         }
     }
 }
