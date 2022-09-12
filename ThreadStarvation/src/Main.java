3fa59ee6c665b78523650ad88de7133b507d4a99
kamath7 <adithyakamath96@gmail.com>
2022-09-13 00:09:37 +0530

    Thread starvartion

--- ThreadStarvation/src/Main.java
+++ ThreadStarvation/src/Main.java
@@ -4,7 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         Thread t1 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "Priority 10");
+        Thread t2 = new Thread(new Worker(ThreadColor.ANSI_BLUE), "Priority 8");
+        Thread t3 = new Thread(new Worker(ThreadColor.ANSI_RED), "Priority 6");
+        Thread t4 = new Thread(new Worker(ThreadColor.ANSI_PURPLE), "Priority 4");
+        Thread t5 = new Thread(new Worker(ThreadColor.ANSI_WHITE), "Priority 2");
 
+        t1.setPriority(10);
+        t2.setPriority(8);
+        t3.setPriority(6);
+        t4.setPriority(4);
+        t5.setPriority(2);
+
+        t3.start();
+        t2.start();
+        t5.start();
+        t2.start();
+        t1.start();
     }
 
     private static class Worker implements  Runnable {
@@ -19,7 +34,7 @@ public class Main {
         public void run() {
             for (int i = 0; i < 100 ; i++){
                 synchronized (lock){
-                    System.out.format(threadColor + " %s: runCount = %d", Thread.currentThread().getName(), runCount++);
+                    System.out.format(threadColor + " %s: runCount = %d\n", Thread.currentThread().getName(), runCount++);
 
                 }
             }
