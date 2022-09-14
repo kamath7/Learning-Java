c8cc6291c59a50d27c918928f2eb53690529045f
kamath7 <adithyakamath96@gmail.com>
2022-09-14 23:25:21 +0530

    interface

--- Lambdas/src/Main.java
+++ Lambdas/src/Main.java
@@ -33,6 +33,19 @@ public class Main {
         for (Employee employee: employees){
             System.out.println(employee.getName());
         }
+
+        String someString = stringey(new UpperConcat() {
+            @Override
+            public String upperAndConcat(String s1, String s2) {
+                return s1.toUpperCase() + s2.toUpperCase();
+            }
+        }, employees.get(0).getName(), employees.get(1).getName());
+
+        System.out.println(someString);
+    }
+
+    public final static String stringey(UpperConcat uc, String s1, String s2){
+        return uc.upperAndConcat(s1, s2);
     }
 }
 
@@ -60,4 +73,9 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat {
+    public  String upperAndConcat(String s1, String s2);
+
 }
