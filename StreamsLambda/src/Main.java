1d8be4b93d07b83ae89f9f2f76ea824d02ace398
kamath7 <adithyakamath96@gmail.com>
2022-09-17 01:35:24 +0530

    minor change

--- StreamsLambda/src/Main.java
+++ StreamsLambda/src/Main.java
@@ -7,13 +7,13 @@ public class Main {
         List<String> someNumbers = Arrays.asList(
                 "N40","N36",
                 "B12","B8",
-                "G53","G54","G55","G50",
+                "G53","G54","G55","G50","g59",
                 "I26", "I17","I20",
                 "O89"
         );
 
         someNumbers.forEach(number -> {
-            if(number.startsWith("G")){
+            if(number.toUpperCase().startsWith("G")){
                 System.out.println(number);
             }
         });
