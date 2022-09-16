2aabeaba50d722bbe5b901a0abcc3f6208295044
kamath7 <adithyakamath96@gmail.com>
2022-09-17 01:34:05 +0530

    traversing through list

---
+++ StreamsLambda/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> someNumbers = Arrays.asList(
+                "N40","N36",
+                "B12","B8",
+                "G53","G54","G55","G50",
+                "I26", "I17","I20",
+                "O89"
+        );
+
+        someNumbers.forEach(number -> {
+            if(number.startsWith("G")){
+                System.out.println(number);
+            }
+        });
+    }
+}
