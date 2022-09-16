49750c100addac85ce1bb034382e9e3cf8fecf26
kamath7 <adithyakamath96@gmail.com>
2022-09-17 01:42:57 +0530

    Java streams woohoo

--- StreamsLambda/src/Main.java
+++ StreamsLambda/src/Main.java
@@ -1,21 +1,22 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<String> someNumbers = Arrays.asList(
-                "N40","N36",
-                "B12","B8",
-                "G53","G54","G55","G50","g59",
-                "I26", "I17","I20",
-                "O89"
-        );
+        List<String> someNumbers = Arrays.asList("N40", "N36", "B12", "B8", "G53", "G54", "G55", "G50", "g59", "I26", "I17", "I20", "O89");
+        List<String> gNums = new ArrayList<>();
 
-        someNumbers.forEach(number -> {
-            if(number.toUpperCase().startsWith("G")){
-                System.out.println(number);
-            }
-        });
+        someNumbers.stream().map(String::toUpperCase).filter(s -> s.startsWith("G")).sorted().forEach(System.out::println);
+
+        //Standard approach
+//        someNumbers.forEach(number -> {
+//            if (number.toUpperCase().startsWith("G")) {
+//                gNums.add(number);
+//            }
+//        });
+//        gNums.sort((s1, s2) -> s1.compareTo(s2));
+//        gNums.forEach((s) -> System.out.println(s));
     }
 }
