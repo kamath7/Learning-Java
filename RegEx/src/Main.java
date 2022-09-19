858d8b7551a0e0da82588f89f6c70988a07b727f
kamath7 <adithyakamath96@gmail.com>
2022-09-19 21:19:00 +0530

    replaceAll

---
+++ RegEx/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String someString = "The wonderful part of strings are they are strings";
+        System.out.println(someString);
+        String someString2 = someString.replace("The","OO");
+        System.out.println(someString2);
+    }
+}
