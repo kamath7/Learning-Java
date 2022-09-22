f29e436b892cc143fd10942591985bd7d19350bf
kamath7 <adithyakamath96@gmail.com>
2022-09-23 01:50:48 +0530

    using setUp

--- BankingJunit/src/BankAccountTest.java
+++ BankingJunit/src/BankAccountTest.java
@@ -2,9 +2,16 @@ import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    private BankAccount bankAccount;
+
+    @org.junit.Before
+    public void setUp(){
+        bankAccount = bankAccount = new BankAccount("Lalle","Nata",1000, BankAccount.CHECKING);
+        System.out.println("Running");
+    }
+
     @org.junit.Test
     public void deposit() {
-        BankAccount bankAccount = new BankAccount("Lalle","Nata",1000, BankAccount.CHECKING);
         double balance = bankAccount.deposit(200.00D,true);
         assertEquals(1200, balance, 0);
     }
@@ -16,21 +23,18 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void getBalance_deposit() {
-        BankAccount bankAccount = new BankAccount("Lalle","Nata",1000, BankAccount.CHECKING);
         double balance = bankAccount.deposit(200.00D,true);
         assertEquals(1200, bankAccount.getBalance(), 0);
     }
 
     @org.junit.Test
     public void getBalance_withdraw() {
-        BankAccount bankAccount = new BankAccount("Lalle","Nata",1000, BankAccount.CHECKING);
         double balance = bankAccount.withdraw(200.00D,true);
         assertEquals(800, bankAccount.getBalance(), 0);
     }
 
     @org.junit.Test
     public void isChecking_true(){
-        BankAccount bankAccount = new BankAccount("Lalle","Nata",1000, BankAccount.CHECKING);
         assertTrue("Account is not a checking account",bankAccount.isChecking());
     }
 }
