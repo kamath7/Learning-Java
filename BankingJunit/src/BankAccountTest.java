de785a332215e06187ef95d9c48f5a461c520922
kamath7 <adithyakamath96@gmail.com>
2022-09-23 02:05:15 +0530

    minor change

--- BankingJunit/src/BankAccountTest.java
+++ BankingJunit/src/BankAccountTest.java
@@ -6,19 +6,19 @@ public class BankAccountTest {
     private static int count;
 
     @org.junit.BeforeClass
-    public static void beforeClass(){
-        System.out.println("Executes once before test cases. Count "+count++);
+    public static void beforeClass() {
+        System.out.println("Executes once before test cases. Count " + count++);
     }
 
     @org.junit.Before
     //before each test
-    public void setUp(){
-        bankAccount = bankAccount = new BankAccount("Lalle","Nata",1000, BankAccount.CHECKING);
+    public void setUp() {
+        bankAccount = bankAccount = new BankAccount("Lalle", "Nata", 1000, BankAccount.CHECKING);
     }
 
     @org.junit.Test
     public void deposit() {
-        double balance = bankAccount.deposit(200.00D,true);
+        double balance = bankAccount.deposit(200.00D, true);
         assertEquals(1200, balance, 0);
     }
 
@@ -30,35 +30,34 @@ public class BankAccountTest {
     }
 
     @org.junit.Test(expected = IllegalArgumentException.class)
-    public void withdraw_no_branch() throws Exception{
-        double balance = bankAccount.withdraw(600, false);
-        assertEquals(1200, balance, 0);
+    public void withdraw_no_branch() throws Exception {
+        bankAccount.withdraw(600, false);
     }
 
     @org.junit.Test
     public void getBalance_deposit() {
-        double balance = bankAccount.deposit(200.00D,true);
+        double balance = bankAccount.deposit(200.00D, true);
         assertEquals(1200, bankAccount.getBalance(), 0);
     }
 
     @org.junit.Test
     public void getBalance_withdraw() {
-        double balance = bankAccount.withdraw(200.00D,true);
+        double balance = bankAccount.withdraw(200.00D, true);
         assertEquals(800, bankAccount.getBalance(), 0);
     }
 
     @org.junit.Test
-    public void isChecking_true(){
-        assertTrue("Account is not a checking account",bankAccount.isChecking());
+    public void isChecking_true() {
+        assertTrue("Account is not a checking account", bankAccount.isChecking());
     }
 
     @org.junit.AfterClass
-    public static void afterClass(){
-        System.out.println("After test cases are run! Count "+count++);
+    public static void afterClass() {
+        System.out.println("After test cases are run! Count " + count++);
     }
 
     @org.junit.After
-    public void tearDown(){
+    public void tearDown() {
         count += count;
         System.out.println(count);
     }
