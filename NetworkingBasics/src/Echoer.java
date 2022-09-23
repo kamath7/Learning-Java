34a5f47b853068f8b604e44ecf28702488ef893e
kamath7 <adithyakamath96@gmail.com>
2022-09-24 02:31:58 +0530

    refactoring and overriding the thread's run

---
+++ NetworkingBasics/src/Echoer.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Echoer extends Thread {
+    private Socket socket;
+
+    public Echoer(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            while (true) {
+                String echoStr = input.readLine();
+                if (echoStr.equals("exit")) {
+                    break;
+                }
+                output.println(echoStr);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- NetworkingBasics/src/Main.java
+++ NetworkingBasics/src/Main.java
@@ -1,9 +1,5 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,22 +7,7 @@ public class Main {
         try (ServerSocket serverSocket = new ServerSocket(5000)) {
 
             while (true) {
-                Socket socket = serverSocket.accept();
-                System.out.println("Client connected!");
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream()));
-
-                PrintWriter op = new PrintWriter(socket.getOutputStream(), true);
-                String ehcoStr = bufferedReader.readLine();
-                try {
-                    Thread.sleep(15000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                if (ehcoStr.equals("exit")) {
-                    break;
-                }
-                op.println("Echo from server " + ehcoStr);
+                new Echoer(serverSocket.accept()).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
