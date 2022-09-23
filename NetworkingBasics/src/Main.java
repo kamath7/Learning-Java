494598dd6b7234ef3d0b919d7d26a50f8789076c
kamath7 <adithyakamath96@gmail.com>
2022-09-24 01:28:28 +0530

    server accepting input

--- NetworkingBasics/src/Main.java
+++ NetworkingBasics/src/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,6 +11,18 @@ public class Main {
         try(ServerSocket serverSocket = new ServerSocket(5000)){
             Socket socket = serverSocket.accept();
             System.out.println("Client connected!");
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
+            PrintWriter op = new PrintWriter(socket.getOutputStream(), true);
+
+            while(true){
+                String ehcoStr = bufferedReader.readLine();
+                if (ehcoStr.equals("exit")){
+                    break;
+                }
+                op.println("Echo from server "+ehcoStr);
+            }
         }catch(IOException e){
             e.printStackTrace();
         }
