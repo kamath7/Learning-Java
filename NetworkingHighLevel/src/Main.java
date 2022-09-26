a4f0f31e1da25f3522f250074c1038ec7c1d01bd
kamath7 <adithyakamath96@gmail.com>
2022-09-27 00:30:06 +0530

    multiple uri

--- NetworkingHighLevel/src/Main.java
+++ NetworkingHighLevel/src/Main.java
@@ -7,20 +7,30 @@ public class Main {
     public static void main(String[] args) {
         try {
             URI baseUri = new URI("http://username:password@myserver.com:5000");
-            URI uri = new URI("/catalogue/clothes?shoes=running#nike");
-            URI resolvedURI = baseUri.resolve(uri);
-//            uri = new URI("hello");
-            URL url = resolvedURI.toURL();
-            System.out.println("URL from URI is "+url);
-            System.out.println("Scheme = "+uri.getScheme());
-            System.out.println("Scheme - specific part"+uri.getSchemeSpecificPart());
-            System.out.println("Authority "+uri.getAuthority());
-            System.out.println("User info "+uri.getUserInfo());
-            System.out.println("Host - "+uri.getHost());
-            System.out.println("Port - "+uri.getPort());
-            System.out.println("Path "+uri.getPath());
-            System.out.println("Query "+uri.getQuery());
-            System.out.println("Fragment "+uri.getFragment());
+            URI uri1 = new URI("/catalogue/clothes?shoes=running#nike");
+            URI uri2 = new URI("/catalogue/tvs?led=gppd#sansui");
+            URI uri3 = new URI("/catalogue/phones?os=android#moto");
+            URI resolvedURI1 = baseUri.resolve(uri1);
+            URI resolvedURI2 = baseUri.resolve(uri2);
+            URI resolvedURI3 = baseUri.resolve(uri3);
+
+            URL url1 = resolvedURI1.toURL();
+            System.out.println("URL from URI1 is "+url1);
+
+            URL url2 = resolvedURI2.toURL();
+            System.out.println("URL from URI2 is "+url2);
+
+            URL url3 = resolvedURI3.toURL();
+            System.out.println("URL from URI3 is "+url3);
+//            System.out.println("Scheme = "+uri.getScheme());
+//            System.out.println("Scheme - specific part"+uri.getSchemeSpecificPart());
+//            System.out.println("Authority "+uri.getAuthority());
+//            System.out.println("User info "+uri.getUserInfo());
+//            System.out.println("Host - "+uri.getHost());
+//            System.out.println("Port - "+uri.getPort());
+//            System.out.println("Path "+uri.getPath());
+//            System.out.println("Query "+uri.getQuery());
+//            System.out.println("Fragment "+uri.getFragment());
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }catch(MalformedURLException e){
