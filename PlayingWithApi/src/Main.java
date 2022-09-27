0ba8bdea4c5f2721a5e622ced4e2a0b4d28deb30
kamath7 <adithyakamath96@gmail.com>
2022-09-27 20:06:50 +0530

    styling

--- PlayingWithApi/src/Main.java
+++ PlayingWithApi/src/Main.java
@@ -8,34 +8,31 @@ import java.net.URL;
 public class Main {
     public static void main(String[] args) {
 
-        try{
+        try {
             URL url = new URL("https://www.flickr.com/services/feeds/photos_public.gne?tags=dogs");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
-            connection.setRequestProperty("User-Agent","Chrome");
+            connection.setRequestProperty("User-Agent", "Chrome");
             connection.setReadTimeout(30000);
 
             int resCode = connection.getResponseCode();
-            System.out.println("Response code "+resCode);
+            System.out.println("Response code " + resCode);
 
-            if(resCode != 200){
+            if (resCode != 200) {
                 System.out.println("Error reading");
-                System.out.println("Message "+connection.getResponseMessage());
+                System.out.println("Message " + connection.getResponseMessage());
             }
 
             BufferedReader inputReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line = "";
 
-            while((line = inputReader.readLine()) != null){
+            while ((line = inputReader.readLine()) != null) {
                 System.out.println(line);
             }
-        }catch(MalformedURLException e){
-                e.printStackTrace();
-        }catch(IOException e){
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
     }
 }
