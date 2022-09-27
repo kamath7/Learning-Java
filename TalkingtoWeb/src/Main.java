c4f26c82605bf55f7d2f68ae9d52bc4d0fce5670
kamath7 <adithyakamath96@gmail.com>
2022-09-27 18:22:33 +0530

    getting url info

--- TalkingtoWeb/src/Main.java
+++ TalkingtoWeb/src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,12 +18,23 @@ public class Main {
 
             BufferedReader inputStream = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
             //            URI uri = url.toURI();
-            String line = "";
-            while (line != null) {
-                line = inputStream.readLine();
-                System.out.println(line);
+
+            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+            for(Map.Entry<String, List<String>> entry: headerFields.entrySet()){
+                String key = entry.getKey();
+                List<String> val = entry.getValue();
+                System.out.println("----Key -> "+key);
+
+                for(String string: val){
+                    System.out.println("Value -> "+val);
+                }
             }
-            inputStream.close();
+//            String line = "";
+//            while (line != null) {
+//                line = inputStream.readLine();
+//                System.out.println(line);
+//            }
+//            inputStream.close();
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
